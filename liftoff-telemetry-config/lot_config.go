e4f6d83b7181b28518f0c6e9ab8c844d19c337c2
Dmytro Lapko <dmitriy.lapko@acentio.com>
2026-02-12 23:35:47 +0200

    Read configured fields from TelemetryConfiguration.json

--- liftoff-telemetry-config/lot_config.go
+++ liftoff-telemetry-config/lot_config.go
@@ -8,9 +8,67 @@ import (
 	"os"
 )
 
+type StreamDataType int
+
+const (
+	Timestamp StreamDataType = iota
+	Position
+	Attitude
+	Velocity
+	Gyro
+	Input
+	Battery
+	MotorRPM
+	Unknown
+)
+
 type LiftoffTelemetryConfig struct {
-	EndPoint     string   `json:"EndPoint"`
-	StreamFormat []string `json:"StreamFormat"`
+	Endpoint          string   `json:"EndPoint"`
+	StreamFormatNames []string `json:"StreamFormat"`
+	StreamFormats     []StreamDataType
+	StreamFormatsMap  map[StreamDataType]string
+}
+
+func (t LiftoffTelemetryConfig) HasStreamDataType(dataType StreamDataType) bool {
+	_, ok := t.StreamFormatsMap[dataType]
+	return ok
+}
+
+func (t LiftoffTelemetryConfig) HasPosition() bool {
+	return t.HasStreamDataType(Position)
+}
+func (t LiftoffTelemetryConfig) HasVelocity() bool {
+	return t.HasStreamDataType(Velocity)
+}
+
+func (t *LiftoffTelemetryConfig) UpdateStreamFormats() {
+	t.StreamFormats = make([]StreamDataType, len(t.StreamFormatNames))
+	t.StreamFormatsMap = map[StreamDataType]string{}
+	for i, name := range t.StreamFormatNames {
+		var ts StreamDataType
+		switch name {
+		case "Timestamp":
+			ts = Timestamp
+		case "Position":
+			ts = Position
+		case "Attitude":
+			ts = Attitude
+		case "Velocity":
+			ts = Velocity
+		case "Gyro":
+			ts = Gyro
+		case "Input":
+			ts = Input
+		case "Battery":
+			ts = Battery
+		case "MotorRPM":
+			ts = MotorRPM
+		default:
+			ts = Unknown
+		}
+		t.StreamFormats[i] = ts
+		t.StreamFormatsMap[ts] = name
+	}
 }
 
 func ReadConfig() (*LiftoffTelemetryConfig, error) {
@@ -31,6 +89,7 @@ func ReadConfig() (*LiftoffTelemetryConfig, error) {
 
 	var config LiftoffTelemetryConfig
 	json.Unmarshal(bytes, &config)
+	config.UpdateStreamFormats()
 
 	return &config, nil
 }
