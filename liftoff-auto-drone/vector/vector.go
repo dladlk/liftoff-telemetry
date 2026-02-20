2be20f31b3791c55f73322e7d673d9085e52f397
Dmytro Lapko <dmitriy.lapko@acentio.com>
2026-02-20 15:54:53 +0200

    Add several more functions to vector

--- liftoff-auto-drone/vector/vector.go
+++ liftoff-auto-drone/vector/vector.go
@@ -1,9 +1,25 @@
 package vector
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"strings"
+)
 
 func VectorPrint(name string, v [4]float32) string {
 	return fmt.Sprintf("%s [% .6f % .6f % .6f % .6f]", name, v[0], v[1], v[2], v[3])
+}
+
+func VectorPrintByDecimal(name string, v [4]float32, decimalsAfterComma int) string {
+	format := "%s [% .6f % .6f % .6f % .6f]"
+	if decimalsAfterComma != 6 {
+		format = strings.ReplaceAll(format, "6", fmt.Sprint(decimalsAfterComma))
+	}
+	return fmt.Sprintf(format, name, v[0], v[1], v[2], v[3])
+}
+
+func VectorPrintGyro(name string, v [3]float32) string {
+	return fmt.Sprintf("%s [% .6f % .6f % .6f]", name, v[0], v[1], v[2])
 }
 
 func VectorDiff(v1 [4]float32, v2 [4]float32) [4]float32 {
@@ -12,4 +28,11 @@ func VectorDiff(v1 [4]float32, v2 [4]float32) [4]float32 {
 		diff[i] = v1[i] - v2[i]
 	}
 	return diff
+}
+func VectorZero(v []float32) bool {
+	var absSum float64
+	for _, vv := range v {
+		absSum += math.Abs(float64(vv))
+	}
+	return absSum < 0.000001
 }
--- liftoff-telemetry-analyzer/main.go
+++ liftoff-telemetry-analyzer/main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	track "github.com/dladlk/liftoff-auto-drone/track"
+	vector "github.com/dladlk/liftoff-auto-drone/vector"
 )
 
 func main() {
@@ -28,5 +29,22 @@ func main() {
 			fmt.Printf("Failed to read track file %s: %v", path, err)
 			os.Exit(1)
 		}
+
+		printLimit := 100
+		zeroRPMCount := 0
+		printCount := 0
+		for _, d := range t.List {
+			if vector.VectorZero(d.MotorRPM[:]) {
+				zeroRPMCount++
+				continue
+			}
+			fmt.Println(vector.VectorPrint("Attitude", d.Attitude), vector.VectorPrintGyro("Gyro", d.Gyro), vector.VectorPrintByDecimal("RPM", [4]float32(d.MotorRPM), 1))
+			printCount++
+			if printCount >= printLimit {
+				break
+			}
+		}
+		fmt.Printf("Printed %d frames, skipped %d", printCount, zeroRPMCount)
 	}
+
 }
